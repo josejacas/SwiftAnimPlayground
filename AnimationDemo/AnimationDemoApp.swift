1f3bbf0b8c9e6adb7b91782b825b98f426ad9990
Jose <jmjacas@gmail.com>
2025-12-12 11:24:34 +0100

    Initial Commit

---
+++ AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by Jose Saez-Merino on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
